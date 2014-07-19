73325dcbef42f112c7b1fbf50850b93894b3fd93
Jon Binney <jon.binney@gmail.com>
2014-07-18 18:24:38 -0700

    Add option to use multiple threads

--- apriltags_ros/src/apriltag_detector_node.cpp
+++ apriltags_ros/src/apriltag_detector_node.cpp
@@ -5,6 +5,19 @@ int main(int argc, char **argv){
   ros::init(argc, argv, "apriltag_detector");
   ros::NodeHandle nh;
   ros::NodeHandle pnh("~");
+
   apriltags_ros::AprilTagDetector detector(nh, pnh);
-  ros::spin();
+
+  // Check whether to run multiple detection threads
+  int num_threads;
+  pnh.param("num_threads", num_threads, 1);
+  if(num_threads > 1){
+    ROS_INFO("Starting multi-threaded spinner with %d threads", num_threads);
+    ros::AsyncSpinner spinner(num_threads);
+    spinner.start();
+    ros::waitForShutdown();
+  }
+  else{
+    ros::spin();
+  }
 }
